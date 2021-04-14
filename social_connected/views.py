4794c209482534dafd2d7bb4f3dcb644397bd068
Wagner de Lima <waglds@gmail.com>
2021-04-14 15:12:58 +0200

    feat: add twitter view and url endpoint.

--- social_connected/views.py
+++ social_connected/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
 
-# Create your views here.
+from social_connected.controller_logic.twitter_connected import TwitterConnected
+
+
+class SocialConnected(generics.RetrieveAPIView):
+    lookup_fields = ['source_dev', 'target_dev']
+
+    def get(self, request, *args, **kwargs):
+        url_params = {field: self.kwargs[field] for field in self.lookup_fields}
+        twitter_connected = TwitterConnected(**url_params)
+        response = twitter_connected.connected()
+        return Response(response)
