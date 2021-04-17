035f6c8a30bfa2e51dc65b305dc103d1b69c2f81
Wagner de Lima <waglds@gmail.com>
2021-04-18 00:23:51 +0200

    feat: add register endpoint

--- social_connected/views.py
+++ social_connected/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from rest_framework.response import Response
 
+from social_connected.controller_logic.registry import Registry
 from social_connected.controller_logic.social_connected import SocialConnected
 
 
@@ -16,4 +17,19 @@ class SocialConnectedView(generics.RetrieveAPIView):
         }
         social_connected = SocialConnected(**url_params)
         response, status = social_connected.connected()
+        return Response(response, status=status)
+
+
+class RegistryView(generics.RetrieveAPIView):
+    lookup_fields = ['source_dev', 'target_dev']
+
+    def get(self, request, *args, **kwargs):
+        """
+        Retrieve registry of both source and target developers.
+        """
+        url_params = {
+            field: self.kwargs[field] for field in self.lookup_fields
+        }
+        social_connected = Registry(**url_params)
+        response, status = social_connected.retrieve_registries()
         return Response(response, status=status)
